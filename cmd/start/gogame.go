beea323753cb8012785363c0785831c8372f0023
Andrew Baumes <andrewbaumes@gmail.com>
2018-03-20 16:14:55 -0400

    added a python launcher for now

--- cmd/start/gogame.go
+++ cmd/start/gogame.go
@@ -13,77 +13,107 @@ import (
 )
 
 func main() {
+	args := os.Args[1:]
 	sseed := 1
 	sport := 5555
 	sworld := "default"
 	play := "all"
-
-	var e error
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Enter if you want to do server or client or all (put 'all' or 'server' or 'client'): ")
-
-	playstr, _ := reader.ReadString('\n')
-	if strings.TrimSpace(playstr) == "client" {
-		play = "client"
-	}
-	if strings.TrimSpace(playstr) == "server" {
-		play = "server"
-	}
-	if play == "server" || play == "all" {
-		reader := bufio.NewReader(os.Stdin)
-		fmt.Print("Enter world name for server (leave blank for 'default'): ")
-
-		worldstr, _ := reader.ReadString('\n')
-		if strings.TrimSpace(worldstr) != "" {
-			sworld = worldstr
-		}
-		reader = bufio.NewReader(os.Stdin)
-		fmt.Print("Enter seed for server (leave blank for 1): ")
-		seedstr, _ := reader.ReadString('\n')
-		if strings.TrimSpace(seedstr) != "" {
-			sseed, e = strconv.Atoi(strings.TrimSpace(seedstr))
-			if e != nil {
-				panic(e)
-			}
-		}
-		reader = bufio.NewReader(os.Stdin)
-		fmt.Print("Enter port for server (leave blank for 5555): ")
-		portstr, _ := reader.ReadString('\n')
-		if strings.TrimSpace(portstr) != "" {
-			sport, e = strconv.Atoi(strings.TrimSpace(portstr))
-			if e != nil {
-				panic(e)
-			}
-		}
-	}
 	name := "andrew"
 	host := "localhost"
 	port := 5555
-	if play == "client" || play == "all" {
-		reader := bufio.NewReader(os.Stdin)
-		fmt.Print("Enter your name DO NOT LEAVE BLANK: ")
 
-		namestr, _ := reader.ReadString('\n')
-		if strings.TrimSpace(namestr) != "" {
-			name = namestr
+	var e error
+	if len(args) == 0 {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("Enter if you want to do server or client or all (put 'all' or 'server' or 'client'): ")
+
+		playstr, _ := reader.ReadString('\n')
+		if strings.TrimSpace(playstr) == "client" {
+			play = "client"
 		}
-		reader = bufio.NewReader(os.Stdin)
-		fmt.Print("Enter host for client (leave blank for 'localhost'): ")
-		hoststr, _ := reader.ReadString('\n')
-		if strings.TrimSpace(hoststr) != "" {
-			host = strings.TrimSpace(hoststr)
+		if strings.TrimSpace(playstr) == "server" {
+			play = "server"
 		}
-		reader = bufio.NewReader(os.Stdin)
-		fmt.Print("Enter port for client (leave blank for 5555): ")
-		portstr, _ := reader.ReadString('\n')
-		if strings.TrimSpace(portstr) != "" {
-			port, e = strconv.Atoi(strings.TrimSpace(portstr))
-			if e != nil {
-				panic(e)
+		if play == "server" || play == "all" {
+			reader := bufio.NewReader(os.Stdin)
+			fmt.Print("Enter world name for server (leave blank for 'default'): ")
+
+			worldstr, _ := reader.ReadString('\n')
+			if strings.TrimSpace(worldstr) != "" {
+				sworld = worldstr
+			}
+			reader = bufio.NewReader(os.Stdin)
+			fmt.Print("Enter seed for server (leave blank for 1): ")
+			seedstr, _ := reader.ReadString('\n')
+			if strings.TrimSpace(seedstr) != "" {
+				sseed, e = strconv.Atoi(strings.TrimSpace(seedstr))
+				if e != nil {
+					panic(e)
+				}
+			}
+			reader = bufio.NewReader(os.Stdin)
+			fmt.Print("Enter port for server (leave blank for 5555): ")
+			portstr, _ := reader.ReadString('\n')
+			if strings.TrimSpace(portstr) != "" {
+				sport, e = strconv.Atoi(strings.TrimSpace(portstr))
+				if e != nil {
+					panic(e)
+				}
+			}
+		}
+	} else {
+		play = args[6]
+		sworld = args[0]
+		sport, e = strconv.Atoi(strings.TrimSpace(args[1]))
+		if e != nil {
+			panic(e)
+		}
+		sseed, e = strconv.Atoi(strings.TrimSpace(args[2]))
+		if e != nil {
+			panic(e)
+		}
+		name = args[3]
+		host = args[4]
+		port, e = strconv.Atoi(strings.TrimSpace(args[5]))
+		if e != nil {
+			panic(e)
+		}
+	}
+	if len(args) == 0 {
+		if play == "client" || play == "all" {
+			reader := bufio.NewReader(os.Stdin)
+			fmt.Print("Enter your name DO NOT LEAVE BLANK: ")
+
+			namestr, _ := reader.ReadString('\n')
+			if strings.TrimSpace(namestr) != "" {
+				name = namestr
+			}
+			reader = bufio.NewReader(os.Stdin)
+			fmt.Print("Enter host for client (leave blank for 'localhost'): ")
+			hoststr, _ := reader.ReadString('\n')
+			if strings.TrimSpace(hoststr) != "" {
+				host = strings.TrimSpace(hoststr)
+			}
+			reader = bufio.NewReader(os.Stdin)
+			fmt.Print("Enter port for client (leave blank for 5555): ")
+			portstr, _ := reader.ReadString('\n')
+			if strings.TrimSpace(portstr) != "" {
+				port, e = strconv.Atoi(strings.TrimSpace(portstr))
+				if e != nil {
+					panic(e)
+				}
 			}
 		}
 	}
-	go server.Start(sworld, sseed, sport)
+	if play == "server" || play == "all" {
+		if play == "all" {
+			go server.Start(sworld, sseed, sport)
+		} else {
+			server.Start(sworld, sseed, sport)
+		}
+	}
 	time.Sleep(1)
-	client.Start(name, host, port)
+	if play == "server" || play == "all" {
+		client.Start(name, host, port)
+	}
 }
---
+++ launcher.py
@@ -0,0 +1,166 @@
+import tkinter as tk
+import os
+root = tk.Tk()
+worldd = "default"
+portd = 5555
+seedd = 1
+named = "andrew"
+cportd = 5555
+hostd = "localhost"
+# print('./gogame {} {} {} {} {} {} {}'.format(world, port, seed, "1", "2", "3", "server"))
+def ssumit():
+    global worldd
+    global portd
+    global seedd
+    global named
+    global cportd
+    global hostd
+    worldst = world.get()
+    portst = port.get()
+    seedst = seed.get()
+    if worldst != "":
+        worldd = worldst
+    if portst != "":
+        portd = portst
+    if seedst != "":
+        seedd = seedst
+    # print(worlds, ports, seeds)
+    root.destroy()
+    os.system('./gogame {} {} {} {} {} {} {}'.format(worldd, portd, seedd, "1", "2", "3", "server"))
+def csumit():
+    global worldd
+    global portd
+    global seedd
+    global named
+    global cportd
+    global hostd
+    namest = name.get()
+    hostst = host.get()
+    cportst = cport.get()
+    if worldst != "":
+        named = namest
+    if cportst != "":
+        cportd = cportst
+    if hostst != "":
+        hostd = hostst
+    # print(worlds, ports, seeds)
+    root.destroy()
+    os.system('./gogame {} {} {} {} {} {} {}'.format("1", "1", "1", named, hostd, cportd, "server"))
+def bsumit():
+    global worldd
+    global portd
+    global seedd
+    global named
+    global cportd
+    global hostd
+    worldst = world.get()
+    portst = port.get()
+    seedst = seed.get()
+    if worldst != "":
+        worldd = worldst
+    if portst != "":
+        portd = portst
+    if seedst != "":
+        seedd = seedst
+    namest = name.get()
+    hostst = host.get()
+    cportst = cport.get()
+    if worldst != "":
+        named = namest
+    if cportst != "":
+        cportd = cportst
+    if hostst != "":
+        hostd = hostst
+    root.destroy()
+    os.system('./gogame {} {} {} {} {} {} {}'.format(worldd, portd, seedd, named, hostd, cportd, "all"))
+def server():
+    global world
+    global port
+    global seed
+    global name
+    global cport
+    global host
+    runs.destroy()
+    runs1.destroy()
+    runs2.destroy()
+    world = tk.Entry(root)
+    worldl = tk.Label(root, text="enter you world name leave blank for default" )
+    port = tk.Entry(root)
+    portl = tk.Label(root, text="enter you port for the server leave blank for 5555")
+    seed = tk.Entry(root)
+    seedl = tk.Label(root, text="enter the seed for the world")
+    worldl.pack()
+    world.pack()
+    portl.pack()
+    port.pack()
+    seedl.pack()
+    seed.pack()
+    sumit = tk.Button(root, text="summit", command=ssumit)
+    sumit.pack()
+def client():
+    global world
+    global port
+    global seed
+    global name
+    global cport
+    global host
+    runs.destroy()
+    runs1.destroy()
+    runs2.destroy()
+    name = tk.Entry(root)
+    namel = tk.Label(root, text="enter your name DO NO LEAVE BLANK" )
+    cport = tk.Entry(root)
+    cportl = tk.Label(root, text="enter you port for the client leave blank for 5555")
+    host = tk.Entry(root)
+    hostl = tk.Label(root, text="enter the host for the server")
+    namel.pack()
+    name.pack()
+    cportl.pack()
+    cport.pack()
+    hostl.pack()
+    host.pack()
+    sumit = tk.Button(root, text="summit", command=csumit)
+    sumit.pack()
+def both():
+    global world
+    global port
+    global seed
+    global name
+    global cport
+    global host
+    runs.destroy()
+    runs1.destroy()
+    runs2.destroy()
+    world = tk.Entry(root)
+    worldl = tk.Label(root, text="enter you world name leave blank for default" )
+    port = tk.Entry(root)
+    portl = tk.Label(root, text="enter you port for the server leave blank for 5555")
+    seed = tk.Entry(root)
+    seedl = tk.Label(root, text="enter the seed for the world")
+    name = tk.Entry(root)
+    namel = tk.Label(root, text="enter your name DO NO LEAVE BLANK" )
+    cport = tk.Entry(root)
+    cportl = tk.Label(root, text="enter you port for the client leave blank for 5555")
+    host = tk.Entry(root)
+    hostl = tk.Label(root, text="enter the host for the server")
+    cportl.pack()
+    cport.pack()
+    namel.pack()
+    name.pack()
+    hostl.pack()
+    host.pack()
+    worldl.pack()
+    world.pack()
+    portl.pack()
+    port.pack()
+    seedl.pack()
+    seed.pack()
+    sumit = tk.Button(root, text="summit", command=bsumit)
+    sumit.pack()
+runs = tk.Button(root, text="server", command=server)
+runs.pack()
+runs1 = tk.Button(root, text="client", command=client)
+runs1.pack()
+runs2 = tk.Button(root, text="both", command=both)
+runs2.pack()
+root.mainloop()
