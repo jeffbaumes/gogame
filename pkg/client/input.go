4d8a2eeb4167dfcd22703bed41ed2c86caaeefb1
Jeffrey Baumes <jeff.baumes@kitware.com>
2018-04-29 23:55:58 -0400

    Some better options

--- pkg/client/input.go
+++ pkg/client/input.go
@@ -65,30 +65,85 @@ var (
 	guistart   = time.Now()
 )
 
+func keyCallbackInventory(w *glfw.Window, key glfw.Key, scancode int, action glfw.Action, mods glfw.ModifierKey) {
+	if action != glfw.Press {
+		return
+	}
+	player := universe.Player
+	slot := -1
+	m := op.OptionMap
+	switch key {
+	case m["Slot1"].Key:
+		slot = 0
+	case m["Slot2"].Key:
+		slot = 1
+	case m["Slot3"].Key:
+		slot = 2
+	case m["Slot4"].Key:
+		slot = 3
+	case m["Slot5"].Key:
+		slot = 4
+	case m["Slot6"].Key:
+		slot = 5
+	case m["Slot7"].Key:
+		slot = 6
+	case m["Slot8"].Key:
+		slot = 7
+	case m["Slot9"].Key:
+		slot = 8
+	case m["Slot10"].Key:
+		slot = 9
+	case m["Slot11"].Key:
+		slot = 10
+	case m["Slot12"].Key:
+		slot = 11
+	}
+	if slot < 0 {
+		return
+	}
+	xpos, ypos := w.GetCursorPos()
+	winw, winh := w.GetSize()
+	aspect := float32(winw) / float32(winh)
+	for m := range common.Materials {
+		sz := float32(0.03)
+		px := 1.25 * 2 * sz * (float32(m) - float32(len(common.Materials))/2)
+		py := 1 - 0.25*aspect
+		scale := sz
+		xMin, yMin := glToPixel(w, float64(px-scale), float64(py+scale*aspect))
+		xMax, yMax := glToPixel(w, float64(px+scale), float64(py-scale*aspect))
+		if float64(xpos) >= xMin && float64(xpos) <= xMax && float64(ypos) >= yMin && float64(ypos) <= yMax {
+			player.Hotbar[slot] = m
+		}
+	}
+}
+
 func keyCallbackPlay(w *glfw.Window, key glfw.Key, scancode int, action glfw.Action, mods glfw.ModifierKey) {
 	player := universe.Player
+	planet := player.Planet
+	planetRen := universe.PlanetMap[planet.ID]
+	m := op.OptionMap
 	switch action {
 	case glfw.Press:
 		switch key {
 		case glfw.KeyEscape:
 			player.Mode = "Options"
 			w.SetInputMode(glfw.CursorMode, glfw.CursorNormal)
-		case op.InSky:
+		case m["Apex"].Key:
 			player.Mode = "Apex"
-		case op.OpText:
+		case m["Text"].Key:
 			player.Mode = "Text"
-		case op.Inventory:
+		case m["Inventory"].Key:
 			player.Mode = "Inventory"
 			w.SetInputMode(glfw.CursorMode, glfw.CursorNormal)
-		case op.Forwards:
+		case m["Forward"].Key:
 			player.ForwardVel = player.WalkVel
-		case op.Backwards:
+		case m["Backward"].Key:
 			player.BackVel = player.WalkVel
-		case op.Right:
+		case m["Right"].Key:
 			player.RightVel = player.WalkVel
-		case op.Left:
+		case m["Left"].Key:
 			player.LeftVel = player.WalkVel
-		case op.Mode:
+		case m["Mode"].Key:
 			player.GameMode++
 			if player.GameMode >= common.NumGameModes {
 				player.GameMode = 0
@@ -96,14 +151,31 @@ func keyCallbackPlay(w *glfw.Window, key glfw.Key, scancode int, action glfw.Act
 			if player.GameMode == common.Flying {
 				player.FallVel = 0
 			}
-		case op.PlanetR:
-			id := player.Planet.ID + 1
-			if universe.PlanetMap[id] == nil {
-				id = 0
-			}
-			player.Planet = universe.PlanetMap[id].Planet
-			player.Spawn()
-		case op.Up:
+		case m["Slot1"].Key:
+			player.ActiveHotBarSlot = 0
+		case m["Slot2"].Key:
+			player.ActiveHotBarSlot = 1
+		case m["Slot3"].Key:
+			player.ActiveHotBarSlot = 2
+		case m["Slot4"].Key:
+			player.ActiveHotBarSlot = 3
+		case m["Slot5"].Key:
+			player.ActiveHotBarSlot = 4
+		case m["Slot6"].Key:
+			player.ActiveHotBarSlot = 5
+		case m["Slot7"].Key:
+			player.ActiveHotBarSlot = 6
+		case m["Slot8"].Key:
+			player.ActiveHotBarSlot = 7
+		case m["Slot9"].Key:
+			player.ActiveHotBarSlot = 8
+		case m["Slot10"].Key:
+			player.ActiveHotBarSlot = 9
+		case m["Slot11"].Key:
+			player.ActiveHotBarSlot = 10
+		case m["Slot12"].Key:
+			player.ActiveHotBarSlot = 11
+		case m["Up"].Key:
 			if cursorGrabbed(w) {
 				if player.GameMode == common.Normal {
 					player.HoldingJump = true
@@ -111,11 +183,18 @@ func keyCallbackPlay(w *glfw.Window, key glfw.Key, scancode int, action glfw.Act
 					player.UpVel = player.WalkVel
 				}
 			}
-		case op.Down:
+		case m["Down"].Key:
 			if cursorGrabbed(w) && player.GameMode == common.Flying {
 				player.DownVel = player.WalkVel
 			}
-		case op.PlanetL:
+		case m["PlanetR"].Key:
+			id := player.Planet.ID + 1
+			if universe.PlanetMap[id] == nil {
+				id = 0
+			}
+			player.Planet = universe.PlanetMap[id].Planet
+			player.Spawn()
+		case m["PlanetL"].Key:
 			id := player.Planet.ID - 1
 			if universe.PlanetMap[id] == nil {
 				if id < 0 {
@@ -128,57 +207,7 @@ func keyCallbackPlay(w *glfw.Window, key glfw.Key, scancode int, action glfw.Act
 			}
 			player.Planet = universe.PlanetMap[id].Planet
 			player.Spawn()
-		}
-	case glfw.Release:
-		switch key {
-		case op.Up:
-			player.HoldingJump = false
-			player.UpVel = 0
-		case op.Down:
-			player.DownVel = 0
-		case op.Forwards:
-			player.ForwardVel = 0
-		case op.Backwards:
-			player.BackVel = 0
-		case op.Right:
-			player.RightVel = 0
-		case op.Left:
-			player.LeftVel = 0
-		}
-	}
-}
-
-func keyCallback(w *glfw.Window, key glfw.Key, scancode int, action glfw.Action, mods glfw.ModifierKey) {
-	player := universe.Player
-	guikeycallback := screen.KeyCallBack()
-	m := player.Mode
-	if m == "Play" {
-		keyCallbackPlay(w, key, scancode, action, mods)
-	} else {
-		if action == glfw.Press && key == glfw.KeyEscape {
-			player.Mode = "Play"
-			w.SetInputMode(glfw.CursorMode, glfw.CursorDisabled)
-		} else if m == "Inventory" || m == "Text" || m == "Options" {
-			guikeycallback(w, key, scancode, action, mods)
-		}
-	}
-}
-
-func windowSizeCallback(w *glfw.Window, wd, ht int) {
-	fwidth, fheight := scene.FramebufferSize(w)
-	gl.Viewport(0, 0, int32(fwidth), int32(fheight))
-}
-
-func mouseButtonCallback(w *glfw.Window, button glfw.MouseButton, action glfw.Action, mods glfw.ModifierKey) {
-	player := universe.Player
-	planet := player.Planet
-
-	guimousebuttoncallback := screen.MouseButtonCallback()
-	planetRen := universe.PlanetMap[planet.ID]
-	// keyCallbackPlay(w, glfw.Key(button), 0, action, mods)
-	guimousebuttoncallback(w, button, action, mods)
-	if cursorGrabbed(w) {
-		if action == glfw.Press && button == glfw.MouseButtonLeft {
+		case m["Destroy"].Key:
 			increment := player.LookDir().Mul(0.05)
 			pos := player.Location()
 			for i := 0; i < 100; i++ {
@@ -203,7 +232,7 @@ func mouseButtonCallback(w *glfw.Window, button glfw.MouseButton, action glfw.Ac
 					break
 				}
 			}
-		} else if action == glfw.Press && button == glfw.MouseButtonRight {
+		case m["Build"].Key:
 			increment := player.LookDir().Mul(0.05)
 			pos := player.Location()
 			prevCellIndex := common.CellIndex{Lon: -1, Lat: -1, Alt: -1}
@@ -224,10 +253,122 @@ func mouseButtonCallback(w *glfw.Window, button glfw.MouseButton, action glfw.Ac
 				}
 			}
 		}
+	case glfw.Release:
+		switch key {
+		case m["Up"].Key:
+			player.HoldingJump = false
+			player.UpVel = 0
+		case m["Down"].Key:
+			player.DownVel = 0
+		case m["Forward"].Key:
+			player.ForwardVel = 0
+		case m["Backward"].Key:
+			player.BackVel = 0
+		case m["Right"].Key:
+			player.RightVel = 0
+		case m["Left"].Key:
+			player.LeftVel = 0
+		}
+	}
+}
+
+func keyCallback(w *glfw.Window, key glfw.Key, scancode int, action glfw.Action, mods glfw.ModifierKey) {
+	player := universe.Player
+	guikeycallback := screen.KeyCallBack()
+	m := player.Mode
+	if m == "Play" {
+		keyCallbackPlay(w, key, scancode, action, mods)
+	} else {
+		if action == glfw.Press && key == glfw.KeyEscape {
+			player.Mode = "Play"
+			w.SetInputMode(glfw.CursorMode, glfw.CursorDisabled)
+		} else if m == "Inventory" {
+			keyCallbackInventory(w, key, scancode, action, mods)
+		} else if m == "Text" || m == "Options" {
+			guikeycallback(w, key, scancode, action, mods)
+		}
+	}
+}
+
+func windowSizeCallback(w *glfw.Window, wd, ht int) {
+	fwidth, fheight := scene.FramebufferSize(w)
+	gl.Viewport(0, 0, int32(fwidth), int32(fheight))
+}
+
+func mouseButtonCallback(w *glfw.Window, button glfw.MouseButton, action glfw.Action, mods glfw.ModifierKey) {
+	player := universe.Player
+	guimousebuttoncallback := screen.MouseButtonCallback()
+	m := player.Mode
+	if m == "Play" {
+		keyCallbackPlay(w, glfw.Key(button), 0, action, mods)
 	} else {
 		if action == glfw.Press && player.Mode != "Options" {
 			player.Mode = "Play"
 			w.SetInputMode(glfw.CursorMode, glfw.CursorDisabled)
+		} else if m == "Inventory" || m == "Text" || m == "Options" {
+			guimousebuttoncallback(w, button, action, mods)
 		}
 	}
 }
+
+// 	player := universe.Player
+// 	planet := player.Planet
+
+// 	guimousebuttoncallback := screen.MouseButtonCallback()
+// 	planetRen := universe.PlanetMap[planet.ID]
+// 	// keyCallbackPlay(w, glfw.Key(button), 0, action, mods)
+// 	guimousebuttoncallback(w, button, action, mods)
+// 	if cursorGrabbed(w) {
+// 		if action == glfw.Press && button == glfw.MouseButtonLeft {
+// 			increment := player.LookDir().Mul(0.05)
+// 			pos := player.Location()
+// 			for i := 0; i < 100; i++ {
+// 				pos = pos.Add(increment)
+// 				cell := planet.CartesianToCell(pos)
+// 				hitPlayer := false
+// 				for _, otherPlayer := range universe.ConnectedPeople {
+// 					if pos.Sub(otherPlayer.Position).Len() < 0.6 {
+// 						log.Println(fmt.Sprintf("Hit %v", otherPlayer.Name))
+// 						var ret bool
+// 						universe.RPC.Go("API.HitPlayer", common.HitPlayerArgs{From: player.Name, Target: otherPlayer.Name, Amount: 1}, &ret, nil)
+// 						hitPlayer = true
+// 						break
+// 					}
+// 				}
+// 				if hitPlayer {
+// 					break
+// 				}
+// 				if cell != nil && cell.Material != common.Air {
+// 					cellIndex := planet.CartesianToCellIndex(pos)
+// 					planetRen.SetCellMaterial(cellIndex, common.Air, true)
+// 					break
+// 				}
+// 			}
+// 		} else if action == glfw.Press && button == glfw.MouseButtonRight {
+// 			increment := player.LookDir().Mul(0.05)
+// 			pos := player.Location()
+// 			prevCellIndex := common.CellIndex{Lon: -1, Lat: -1, Alt: -1}
+// 			cellIndex := common.CellIndex{}
+// 			for i := 0; i < 100; i++ {
+// 				pos = pos.Add(increment)
+// 				nextCellIndex := planet.CartesianToCellIndex(pos)
+// 				if nextCellIndex != cellIndex {
+// 					prevCellIndex = cellIndex
+// 					cellIndex = nextCellIndex
+// 					cell := planet.CellIndexToCell(cellIndex)
+// 					if cell != nil && cell.Material != common.Air {
+// 						if prevCellIndex.Lon != -1 {
+// 							planetRen.SetCellMaterial(prevCellIndex, player.Hotbar[player.ActiveHotBarSlot], true)
+// 						}
+// 						break
+// 					}
+// 				}
+// 			}
+// 		}
+// 	} else {
+// 		if action == glfw.Press && player.Mode != "Options" {
+// 			player.Mode = "Play"
+// 			w.SetInputMode(glfw.CursorMode, glfw.CursorDisabled)
+// 		}
+// 	}
+// }
--- pkg/scene/options.go
+++ pkg/scene/options.go
@@ -12,13 +12,22 @@ import (
 	"github.com/jeffbaumes/gogame/pkg/common"
 )
 
+// Option holds the information for a key binding option
+type Option struct {
+	Key   glfw.Key
+	entry *gui.Entry
+	label *gui.Label
+}
+
+func newOption(key glfw.Key) *Option {
+	opt := Option{}
+	opt.Key = key
+	return &opt
+}
+
 // Options holds all the user-defined options for controlling the game
 type Options struct {
-	Walk, Run, Left, Right, Forwards, Backwards, FlySprint, Up, Down, Inventory, Slot1, Slot2, Slot3, Slot4, Slot5, Slot6, Slot7, Slot8, Slot9, Slot10, Slot11, Slot12, Mode, OpText, InSky, PlanetR, PlanetL, Destroy, Build glfw.Key
-	walke, runee, lefte, righte, forwardse, backwardse, flysprinte, upe, downe, inventorye, builde, destroye                                                                                                                  *gui.Entry
-	walkl, runl, leftl, rightl, forwardsl, backwardsl, flysprintl, upl, downl, inventoryl, buildl, destroyl                                                                                                                   *gui.Label
-	Everythinge                                                                                                                                                                                                               []*gui.Entry
-	Everythingl                                                                                                                                                                                                               []*gui.Label
+	OptionMap map[string]*Option
 }
 
 func writefile(t string) {
@@ -42,99 +51,86 @@ func load(o *Options) {
 	s := strings.Split(f2, ";")
 	for _, h := range s {
 		g := strings.Split(h, "=")
-		if g[1] == "" {
+		if len(g) < 2 || g[1] == "" {
 			continue
 		}
-		fmt.Printf("'%s'\n", g[1])
 		n, err := strconv.Atoi(g[1])
 		if err != nil {
 			panic(err)
 		}
 		f := glfw.Key(n)
-		switch g[0] {
-		case "forward":
-			o.Forwards = f
-		case "backward":
-			o.Backwards = f
-		case "left":
-			o.Left = f
-		case "right":
-			o.Right = f
-		case "run":
-			o.Run = f
-		case "flyrun":
-			o.FlySprint = f
-		case "up":
-			o.Up = f
-		case "down":
-			o.Down = f
-		case "inventory":
-			o.Inventory = f
-		case "build":
-			o.Build = f
-		case "destroy":
-			o.Destroy = f
-		}
+		o.OptionMap[g[0]].Key = f
 	}
+}
+
+func save(o *Options) {
+	strs := []string{}
+	for name, opt := range o.OptionMap {
+		if opt.entry != nil {
+			opt.Key = opt.entry.Key
+		}
+		strs = append(strs, fmt.Sprintf("%v=%v", name, opt.Key))
+	}
+	writefile(strings.Join(strs, ";"))
 }
 
 // NewOptions creates a new options GUI
 func NewOptions(screen *gui.Screen) *Options {
 	o := Options{}
-	o.InSky = glfw.KeyK
-	o.Mode = glfw.KeyM
-	o.PlanetL = glfw.KeyLeftBracket
-	o.PlanetR = glfw.KeyRightBracket
-	o.OpText = glfw.KeyEnter
-	o.Slot1 = glfw.Key1
-	o.Slot2 = glfw.Key2
-	o.Slot3 = glfw.Key3
-	o.Slot4 = glfw.Key4
-	o.Slot5 = glfw.Key5
-	o.Slot6 = glfw.Key6
-	o.Slot7 = glfw.Key7
-	o.Slot8 = glfw.Key8
-	o.Slot9 = glfw.Key9
-	o.Slot10 = glfw.Key0
-	o.Slot11 = glfw.KeyMinus
-	o.Slot12 = glfw.KeyEqual
-	o.Forwards = glfw.KeyW
-	o.Backwards = glfw.KeyS
-	o.Left = glfw.KeyA
-	o.Right = glfw.KeyD
-	o.Up = glfw.KeySpace
-	o.Down = glfw.KeyLeftShift
-	o.Run = glfw.KeyLeftShift
-	o.FlySprint = glfw.KeyLeftControl
-	o.Inventory = glfw.KeyE
-	o.Build = glfw.Key(1)
-	o.Destroy = glfw.Key(0)
+	o.OptionMap = make(map[string]*Option)
+	m := o.OptionMap
+	m["Apex"] = newOption(glfw.KeyK)
+	m["Mode"] = newOption(glfw.KeyM)
+	m["PlanetL"] = newOption(glfw.KeyLeftBracket)
+	m["PlanetR"] = newOption(glfw.KeyRightBracket)
+	m["Text"] = newOption(glfw.KeyEnter)
+	m["Slot1"] = newOption(glfw.Key1)
+	m["Slot2"] = newOption(glfw.Key2)
+	m["Slot3"] = newOption(glfw.Key3)
+	m["Slot4"] = newOption(glfw.Key4)
+	m["Slot5"] = newOption(glfw.Key5)
+	m["Slot6"] = newOption(glfw.Key6)
+	m["Slot7"] = newOption(glfw.Key7)
+	m["Slot8"] = newOption(glfw.Key8)
+	m["Slot9"] = newOption(glfw.Key9)
+	m["Slot10"] = newOption(glfw.Key0)
+	m["Slot11"] = newOption(glfw.KeyMinus)
+	m["Slot12"] = newOption(glfw.KeyEqual)
+	m["Forward"] = newOption(glfw.KeyW)
+	m["Backward"] = newOption(glfw.KeyS)
+	m["Left"] = newOption(glfw.KeyA)
+	m["Right"] = newOption(glfw.KeyD)
+	m["Up"] = newOption(glfw.KeySpace)
+	m["Down"] = newOption(glfw.KeyLeftShift)
+	m["Run"] = newOption(glfw.KeyLeftShift)
+	m["FlySprint"] = newOption(glfw.KeyLeftControl)
+	m["Inventory"] = newOption(glfw.KeyE)
+	m["Build"] = newOption(glfw.Key(1))
+	m["Destroy"] = newOption(glfw.Key(0))
 	load(&o)
-	o.forwardse = gui.NewKeyEntry(screen, o.Forwards, -0.50, 0.75, 0.2, 0.15, 0.05, nil)
-	o.forwardsl = gui.NewLabel(screen, "Forward:", -0.95, 0.80, 0.09)
-	o.backwardse = gui.NewKeyEntry(screen, o.Backwards, -0.50, 0.55, 0.2, 0.15, 0.05, nil)
-	o.backwardsl = gui.NewLabel(screen, "Backwards:", -0.95, 0.60, 0.09)
-	o.lefte = gui.NewKeyEntry(screen, o.Left, -0.50, 0.35, 0.2, 0.15, 0.05, nil)
-	o.leftl = gui.NewLabel(screen, "Left:", -0.95, 0.40, 0.09)
-	o.righte = gui.NewKeyEntry(screen, o.Right, -0.50, 0.15, 0.2, 0.15, 0.05, nil)
-	o.rightl = gui.NewLabel(screen, "Right:", -0.95, 0.20, 0.09)
-	o.upe = gui.NewKeyEntry(screen, o.Up, 0, 0.75, 0.2, 0.15, 0.05, nil)
-	o.upl = gui.NewLabel(screen, "Up:", -0.25, 0.80, 0.09)
-	o.downe = gui.NewKeyEntry(screen, o.Down, 0, 0.55, 0.2, 0.15, 0.05, nil)
-	o.downl = gui.NewLabel(screen, "Down:", -0.25, 0.60, 0.09)
-	o.runee = gui.NewKeyEntry(screen, o.Run, 0.75, 0.75, 0.2, 0.15, 0.05, nil)
-	o.runl = gui.NewLabel(screen, "Run:", 0.25, 0.80, 0.09)
-	o.flysprinte = gui.NewKeyEntry(screen, o.FlySprint, 0.75, 0.55, 0.2, 0.15, 0.05, nil)
-	o.flysprintl = gui.NewLabel(screen, "FlyRun:", 0.25, 0.60, 0.09)
-	o.inventorye = gui.NewKeyEntry(screen, o.Inventory, 0.75, 0.35, 0.2, 0.15, 0.05, nil)
-	o.inventoryl = gui.NewLabel(screen, "Inventory:", 0.25, 0.40, 0.09)
-	o.builde = gui.NewKeyEntry(screen, o.Build, 0.75, -0.05, 0.2, 0.15, 0.05, nil)
-	o.buildl = gui.NewLabel(screen, "Build:", 0.25, 0, 0.09)
-	o.destroye = gui.NewKeyEntry(screen, o.Destroy, 0.75, 0.15, 0.2, 0.15, 0.05, nil)
-	o.destroyl = gui.NewLabel(screen, "Destroy:", 0.25, 0.20, 0.09)
-	o.Everythinge = append(o.Everythinge, o.forwardse, o.backwardse, o.righte, o.lefte, o.upe, o.downe, o.runee, o.flysprinte, o.inventorye, o.builde, o.destroye)
-	o.Everythingl = append(o.Everythingl, o.forwardsl, o.backwardsl, o.rightl, o.leftl, o.upl, o.downl, o.runl, o.flysprintl, o.inventoryl, o.buildl, o.destroyl)
-	fmt.Printf("%+v", o)
+	m["Forward"].entry = gui.NewKeyEntry(screen, m["Forward"].Key, -0.50, 0.75, 0.2, 0.15, 0.05, nil)
+	m["Forward"].label = gui.NewLabel(screen, "Forward:", -0.95, 0.80, 0.09)
+	m["Backward"].entry = gui.NewKeyEntry(screen, m["Backward"].Key, -0.50, 0.55, 0.2, 0.15, 0.05, nil)
+	m["Backward"].label = gui.NewLabel(screen, "Backward:", -0.95, 0.60, 0.09)
+	m["Left"].entry = gui.NewKeyEntry(screen, m["Left"].Key, -0.50, 0.35, 0.2, 0.15, 0.05, nil)
+	m["Left"].label = gui.NewLabel(screen, "Left:", -0.95, 0.40, 0.09)
+	m["Right"].entry = gui.NewKeyEntry(screen, m["Right"].Key, -0.50, 0.15, 0.2, 0.15, 0.05, nil)
+	m["Right"].label = gui.NewLabel(screen, "Right:", -0.95, 0.20, 0.09)
+	m["Up"].entry = gui.NewKeyEntry(screen, m["Up"].Key, 0, 0.75, 0.2, 0.15, 0.05, nil)
+	m["Up"].label = gui.NewLabel(screen, "Up:", -0.25, 0.80, 0.09)
+	m["Down"].entry = gui.NewKeyEntry(screen, m["Down"].Key, 0, 0.55, 0.2, 0.15, 0.05, nil)
+	m["Down"].label = gui.NewLabel(screen, "Down:", -0.25, 0.60, 0.09)
+	m["Run"].entry = gui.NewKeyEntry(screen, m["Run"].Key, 0.75, 0.75, 0.2, 0.15, 0.05, nil)
+	m["Run"].label = gui.NewLabel(screen, "Run:", 0.25, 0.80, 0.09)
+	m["FlySprint"].entry = gui.NewKeyEntry(screen, m["FlySprint"].Key, 0.75, 0.55, 0.2, 0.15, 0.05, nil)
+	m["FlySprint"].label = gui.NewLabel(screen, "FlySprint:", 0.25, 0.60, 0.09)
+	m["Inventory"].entry = gui.NewKeyEntry(screen, m["Inventory"].Key, 0.75, 0.35, 0.2, 0.15, 0.05, nil)
+	m["Inventory"].label = gui.NewLabel(screen, "Inventory:", 0.25, 0.40, 0.09)
+	m["Build"].entry = gui.NewKeyEntry(screen, m["Build"].Key, 0.75, -0.05, 0.2, 0.15, 0.05, nil)
+	m["Build"].label = gui.NewLabel(screen, "Build:", 0.25, 0, 0.09)
+	m["Destroy"].entry = gui.NewKeyEntry(screen, m["Destroy"].Key, 0.75, 0.15, 0.2, 0.15, 0.05, nil)
+	m["Destroy"].label = gui.NewLabel(screen, "Destroy:", 0.25, 0.20, 0.09)
+	// fmt.Printf("%+v", o)
 	return &o
 }
 
@@ -144,31 +140,15 @@ var oldop string
 func (o *Options) Draw(p *common.Player) {
 	if oldop != p.Mode {
 		oldop = p.Mode
-		o.Forwards = o.forwardse.Key
-		o.Backwards = o.backwardse.Key
-		o.Left = o.lefte.Key
-		o.Right = o.righte.Key
-		o.Inventory = o.inventorye.Key
-		o.Run = o.runee.Key
-		o.FlySprint = o.flysprinte.Key
-		o.Up = o.upe.Key
-		o.Down = o.downe.Key
-		o.Build = o.builde.Key
-		o.Destroy = o.destroye.Key
+		save(o)
 	}
-	if p.Mode == "Options" {
-		for _, x := range o.Everythinge {
-			x.Hide = false
+	hide := p.Mode != "Options"
+	for _, opt := range o.OptionMap {
+		if opt.entry != nil {
+			opt.entry.Hide = hide
 		}
-		for _, x := range o.Everythingl {
-			x.Hide = false
-		}
-	} else {
-		for _, x := range o.Everythinge {
-			x.Hide = true
-		}
-		for _, x := range o.Everythingl {
-			x.Hide = true
+		if opt.label != nil {
+			opt.label.Hide = hide
 		}
 	}
 }
